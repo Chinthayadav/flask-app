9b024d4c7a992a80c8c15f6cce2396ddcaeaa56a
TholemChandraMouli <chandramoulitholem@gmail.com>
2025-07-17 16:56:23 +0530

    Update app.py
    port changed

--- app.py
+++ app.py
@@ -366,4 +366,4 @@ def export_csv():
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', port=8501, use_reloader=False)
